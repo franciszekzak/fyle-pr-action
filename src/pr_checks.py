68012c1a96f699f9406d36d2e77972c2f3306439
Franciszek Żak <franciszekzak@gmail.com>
2023-01-19 02:14:12 +0100

    Use proper token

--- src/pr_checks.py
+++ src/pr_checks.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
 
     pr_checks = PRChecks(
         config_path=os.environ.get("INPUT_CONFIG-FILE"),
-        github_token=os.environ.get("GITHUB_TOKEN"),
+        github_token=os.environ.get("INPUT_GITHUB-TOKEN"),
         event_file=os.environ.get("GITHUB_EVENT_PATH"),
         event_name=os.environ.get("GITHUB_EVENT_NAME"),
         github_url=os.environ.get("GITHUB_API_URL"),
